2df0437f70e90d7a8bca166acfd099d5549399a4
“Shodai <shodai.inose2@gmail.com>
2024-02-28 17:52:40 -0500

    saving eifs

--- HDmediation/R/not_transported.R
+++ HDmediation/R/not_transported.R
@@ -45,7 +45,6 @@ not_transported <- function(data, A, W, Z, M, Y, cens,
 
         ipwy <- ((A == aprime) / gg[, gl("g({aprime}|w)")])*ipcw_ap
         
-        ipwy <- ifelse(ipwy > 100, 100, ipwy)
         if (partial_tmle) {
             fit <- glm(Y ~ 1, offset = qlogis(bb[, gl("b({aprime},Z,M,W)")]), family = "binomial",
                        subset = A == aprime, weights = ipwy * hm #/ mean(ipwy * hm)
@@ -109,7 +108,7 @@ not_transported <- function(data, A, W, Z, M, Y, cens,
 
     val_list <- list(
         results = ans,
-        eif11 = eifs_11, 
-        eif10 = eifs_10, 
-        eif00 = eifs_00)
+        eif11 = eifs$`11`, 
+        eif10 = eifs$`10`, 
+        eif00 = eifs$`00`)
 }
