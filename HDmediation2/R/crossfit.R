9216dc3203a40b3bb190ac9d0d92917f19618b8b
“Shodai <shodai.inose2@gmail.com>
2024-09-11 17:08:26 -0400

    using discrete SL, adjusting bounding

--- HDmediation2/R/crossfit.R
+++ HDmediation2/R/crossfit.R
@@ -5,11 +5,10 @@ crossfit <- function(train, valid, y, type = c("binomial", "continuous"), id = N
                               outcome_type = match.arg(type),
                               folds = NULL,
                               newdata = valid,
-                              group = id,
-                              discrete = FALSE)$preds
+                              group = id)$preds
     lapply(preds, function(x) bound(x))
 }
 
-bound <- function(x, p = 1e-03) {
+bound <- function(x, p = 1e-02) {
     pmax(pmin(x, 1 - p), p)
 }
